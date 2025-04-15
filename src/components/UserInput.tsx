687f7f207a13900669d501c97e37370eb3acd7a0
Manolis <sinadinakis.e@gmail.com>
2025-04-15 13:08:37 +0200

    chore: typescript fix

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -16,27 +16,29 @@ const Label = styled.label`
     text-transform: uppercase;
 `
 
-const Input = styled.input`
+const Input = styled.input.withConfig({
+    shouldForwardProp: (prop) => prop !== '$invalid',
+})<{$invalid?: boolean}>`
     width: 100%;
     padding: 0.5rem;
     border: 1px solid #76c0ae;
     border-radius: 0.25rem;
-    background-color: ${({invalid}) => {
-        return invalid ? '#A52A2A' : 'transparent';
+    background-color: ${({$invalid}) => {
+        return $invalid ? '#A52A2A' : 'transparent';
     }};
     color: '#c2e9e0';
     font-size: 1rem;
-`
+`;
 
 export default function UserInput({onChange, isValid, input}: {
-    onChange: any,
-    isValid: boolean,
     input: {
         initialInvestment: number,
         annualInvestment: number,
         expectedReturn: number,
         duration: number
-    }
+    },
+    isValid: boolean,
+    onChange: (key: string, value: string) => void,
 }) {
     return (
         <SectionInput className='grid grid-cols-2 gap-6 max-w-100'>
@@ -75,7 +77,7 @@ export default function UserInput({onChange, isValid, input}: {
                 <Input
                     type="number"
                     value={input.duration}
-                    invalid={!isValid}
+                    $invalid={!isValid}
                     onChange={
                         (event) => onChange('duration', event.target.value)
                     }
